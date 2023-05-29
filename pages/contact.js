c67e2a84d74fa7d880c76d234229b908ea66639b
rupeshbug <chaulagainrupesh1@gmail.com>
2023-05-29 10:20:47 +0545

    changes made to contact form

--- pages/contact.js
+++ pages/contact.js
@@ -18,6 +18,7 @@ const contact = () => {
             className="block py-2.5 px-0 w-full text-sm text-gray-700 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-600 dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
             placeholder=" "
             required
+            autoComplete="off"
           />
           <label
             htmlFor="floating_email"
@@ -35,6 +36,7 @@ const contact = () => {
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
               placeholder=" "
               required
+              autoComplete="off"
             />
             <label
               htmlFor="floating_first_name"
@@ -51,6 +53,7 @@ const contact = () => {
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
               placeholder=" "
               required
+              autoComplete="off"
             />
             <label
               htmlFor="floating_last_name"
@@ -64,12 +67,12 @@ const contact = () => {
           <div className="relative z-0 w-full mb-6 group">
             <input
               type="tel"
-              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               name="floating_phone"
               id="floating_phone"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
               placeholder=" "
               required
+              autoComplete="off"
             />
             <label
               htmlFor="floating_phone"
@@ -80,15 +83,16 @@ const contact = () => {
           </div>
           <div className="relative z-0 w-full mb-6 group">
             <input
-              type="text"
-              name="floating_company"
-              id="floating_company"
+              type="number"
+              name="floating_weight"
+              id="floating_weight"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
               placeholder=" "
               required
+              autoComplete="off"
             />
             <label
-              htmlFor="floating_company"
+              htmlFor="floating_weight"
               className="peer-focus:font-medium absolute text-sm text-slate-600 dark:text-gray-400 duration-300 transhtmlForm -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-slate-500 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Weight (in kg)
