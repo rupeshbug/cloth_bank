6d4a49976fe87bfc3c74a3975d5a6671b6e3b1b4
rupeshbug <chaulagainrupesh1@gmail.com>
2023-06-15 13:21:30 +0545

    database fixed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
